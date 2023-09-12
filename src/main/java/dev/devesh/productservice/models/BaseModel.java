5eb8fb1efb8293a1b81d73c429d5023e5ddcfc7b
deveshjeewani <deveshjeewani@gmail.com>
2023-09-13 02:00:03 +0530

    Done some changes after coming home 4 dated 12/09/2023

--- src/main/java/dev/devesh/productservice/models/BaseModel.java
+++ src/main/java/dev/devesh/productservice/models/BaseModel.java
@@ -1,6 +1,11 @@
 package dev.devesh.productservice.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseModel {
 
+    @Id
     private Long id;
 }
