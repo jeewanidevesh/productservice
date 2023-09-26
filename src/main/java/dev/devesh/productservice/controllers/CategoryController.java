bfc08bcdbc03579df4894859fde2c31fcb6a0b87
deveshjeewani <deveshjeewani@gmail.com>
2023-09-26 19:19:39 +0530

    Committing work from home on 26/09/23 selfserviceimpl

--- src/main/java/dev/devesh/productservice/controllers/CategoryController.java
+++ src/main/java/dev/devesh/productservice/controllers/CategoryController.java
@@ -31,7 +31,7 @@ public class CategoryController {
             productDto.setDescription(product.getDescription());
             productDto.setTitle(product.getTitle());
             productDto.setImage(product.getImage());
-            productDto.setPrice(product.getPrice());
+            productDto.setPrice(product.getPrice().getPrice());
             productDtos.add(productDto);
 
         }
